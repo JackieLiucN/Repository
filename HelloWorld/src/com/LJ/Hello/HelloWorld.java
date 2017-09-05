9a7ed845ae38d46636ab8741f79eff9a9c26edc9
JackieLiucN <jackie_8902@126.com>
2017-09-06 00:53:03 +0800

    Hello World

---
+++ HelloWorld/src/com/LJ/Hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.LJ.Hello;
+
+public class HelloWorld {
+    public  static  void main(String args[]){
+        System.out.print("Hello World!");
+        System.out.println();
+        System.out.println();
+
+
+    }
+
+}
